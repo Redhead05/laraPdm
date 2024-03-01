7ddc028a8462cfacb87d3100bbcfc659a7202114
vincent <daylightzone91@gmail.com>
2024-03-01 13:36:08 +0700

    update blogDetails.vue

--- resources/js/Pages/blog/blogDetails.vue
+++ resources/js/Pages/blog/blogDetails.vue
@@ -1,7 +1,24 @@
 <script setup lang="js">
+import HeaderComponent from '../../components/header.vue';
+import footerComponent from "../../components/footer.vue";
+import {defineProps} from "vue";
 
+const components = {
+    'header-component': HeaderComponent,
+    'footer-component': footerComponent,
+};
+const props = defineProps({
+    routes:{
+        type: Object,
+        required:true
+    }
+});
 </script>
 
 <template>
-    <h1>KONTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL</h1>
+    <header-component
+        :routes="props.routes"
+    ></header-component>
+
+    <footer-component></footer-component>
 </template>
